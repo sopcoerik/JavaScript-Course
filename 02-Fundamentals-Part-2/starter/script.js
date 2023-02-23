f69bd9df1cf67adf295312c0af143b5c8f426fba
sopcoerik <sopcoerik@gmail.com>
2023-02-23 12:51:20 +0200

    Rewatched functions and exercised

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,80 @@
-'use strict';
+"use strict";
 
+//Video #33
+
+// function logger(name){
+//     console.log(`My name is ${name}`);
+// }
+// logger(`Erik`);
+
+// function fruitProcessor(tomato, potato){
+//     const itsVeggiesReally = `${tomato}es and ${potato}es are veggies undercover`;
+// return itsVeggiesReally;
+// }
+
+// const veggies = fruitProcessor('Tomato', 'Potato');
+// console.log(veggies);
+
+//Video #34
+
+// function calcAge1(birthYear){
+// 	return 2037 - birthYear;
+// }
+
+//  const age1 = calcAge1(2000);
+//  console.log(age1);
+
+//  calcAge2 = function (birthYear){
+// 	return 2037 - birthYear;
+// }
+
+//  const age2 = calcAge1(2000);
+//  console.log(age1);
+
+//Video #35
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(2000);
+// console.log(age3);
+
+// const retirementCalculator = (birthYear) => {
+// 	age = calcAge3(birthYear);
+//  	retirementAge = 65 - age;
+//   return retirementAge;
+// }
+// console.log(retirementCalculator(2000));
+
+//Video #36
+
+// const veggieCutter = (veggieToCut) => {
+//   return veggieToCut * 4;
+// };
+
+// function veggieProcessor(tomato, potato) {
+//   const tomatoPieces = veggieCutter(tomato);
+//   const potatoPieces = veggieCutter(potato);
+//   const veggies = `${tomatoPieces} pieces of tomatoes and ${potatoPieces} pieces of potatoes.`;
+//   return veggies;
+// }
+
+// const veggiePieces = veggieProcessor(4, 5);
+// console.log(veggiePieces);
+
+//Video #38
+
+//Coding Challenge #1 - Functions
+
+// const calcAvg = (a, b, c) => {
+// 	return (a + b + c)/3;
+// }
+
+// const avgDolphins = calcAvg(85, 54, 41);
+// const avgKoalas = calcAvg(23, 34, 27);
+
+// const checkWinner = (avgDolphins, avgKoalas) => {
+// 	return avgDolphins > avgKoalas*2 ? winner = `Dolphins win (${avgDolphins} vs ${avgKoalas})` : avgKoalas > avgDolphins*2 ? winner = `Dolphins win (${avgDolphins} vs ${avgKoalas})` : winner = `No winner`;
+// }
+
+// const winnerTeam = checkWinner(avgDolphins, avgKoalas);
+
+// console.log(winnerTeam);
