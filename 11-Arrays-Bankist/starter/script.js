0a0a22afc1c0ccc207f48107efbb9b0267f472d3
sopcoerik <sopcoerik@gmail.com>
2023-03-06 09:03:30 +0200

    added arrays test

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,76 +1,128 @@
-'use strict';
+'strict mode';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// BANKIST APP
+//Challenge #1
 
-// Data
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
+// const checkDogs = (dogsJulia, dogsKate) => {
+//  //1
+//   dogsJuliaCorrect = dogsJulia.slice();
+//   dogsJuliaCorrect.splice(0, 1);
+//   dogsJuliaCorrect.splice(-2);
+//      console.log(dogsJuliaCorrect)
+//  //2
+//   allDogs = [...dogsJuliaCorrect, ...dogsKate];
+//   console.log(allDogs);
+//  //3
+//   allDogs.forEach((dogAge, index) =>
+//     dogAge < 3
+//       ? console.log(`Dog number ${index + 1} is still a puppy 🐶`)
+//       : console.log(
+//           `Dog number ${index + 1} is an adult, and is ${dogAge} years old`
+//         )
+//   );
+// };
 
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
+// // Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+// // Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+//  //4
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+// ---------------------------------------------------------------
 
-const accounts = [account1, account2, account3, account4];
+//Challenge #2
 
-// Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector('.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+// const calcAverageHumanAge = ages => {
+//  //1
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//      console.log(humanAges);
+//  //2
+//   const humanAgesFiltered = humanAges.filter(age => age >= 18);
+//      console.log(humanAgesFiltered);
+//  //3
+//   const averageHumanAge =
+//     humanAgesFiltered.reduce((accumulator, age) => accumulator + age, 0) /
+//     humanAgesFiltered.length;
+//   return averageHumanAge;
+// };
+//  //4
+//console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+//console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+//Challenge #3
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+// const calcAverageHumanAge = ages => {
+//   const humanAges = ages
+//     .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//     .filter(age => age >= 18);
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector('.form__input--amount');
-const inputLoanAmount = document.querySelector('.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+//   const avgHumanAges =
+//     humanAges.reduce((accumulator, age) => accumulator + age, 0) /
+//     humanAges.length;
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+//   return avgHumanAges;
+// };
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//Challenge #4
 
-/////////////////////////////////////////////////
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+// ];
+
+// //1
+// dogs.forEach(dog => (dog.recommendedFood = dog.weight ** 0.75 * 28));
+// console.log(dogs);
+
+// //2
+// sarahsDog = dogs.find(dog => dog.owners.includes('Sarah'));
+// console.log(sarahsDog);
+// console.log(
+//   `Sarah\'s dog eats too ${
+//     sarahsDog.curFood > sarahsDog.recommendedFood ? 'much' : 'little'
+//   }`
+// );
+
+// //3
+// const ownersEatTooMuch = dogs
+//   .map(dog => (dog.curFood > dog.recommendedFood ? dog.owners : false))
+//   .filter(dog => dog !== false)
+//   .flat();
+// const ownersEatTooLittle = dogs
+//   .map(dog => (dog.curFood < dog.recommendedFood ? dog.owners : false))
+//   .filter(dog => dog !== false)
+//   .flat();
+
+// //4
+// console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much`);
+// console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little`);
+
+// //5
+// console.log(dogs.some(dog => dog.curFood === dog.recommendedFood));
+
+// //6
+// console.log(
+//   dogs.some(
+//     dog =>
+//       dog.curFood > dog.recommendedFood * 0.9 &&
+//       dog.curFood < dog.recommendedFood * 1.1
+//   )
+// );
+
+// //7
+// const ownersEatOkay = dogs.filter(
+//   dog =>
+//     dog.curFood > dog.recommendedFood * 0.9 &&
+//     dog.curFood < dog.recommendedFood * 1.1
+// );
+// console.log(ownersEatOkay);
+
+// //8
+// const shallowCopyDogs = dogs.map(dog => dog);
+// shallowCopyDogs.sort((a, b) => a.recommendedFood - b.recommendedFood);
+// console.log(shallowCopyDogs);
