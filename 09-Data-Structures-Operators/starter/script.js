127e4697441c7627ab3863a1485923f8e498b156
sopcoerik <sopcoerik@gmail.com>
2023-03-06 09:04:17 +0200

    added data struct coding challenge

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 // // Data needed for a later exercise
-// const flights =
-//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights.split('+'));
 
 // // Data needed for first part of the section
 
@@ -298,3 +300,44 @@
 
 //3
 // console.log(`An event happened every ${90 / gameEvents.size} minutes`);
+
+//Coding challenge #4
+
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// const bigArray = [];
+
+//Coding Challenge #4
+
+//My solution
+const camelCaseConvert = words => {
+  words.trim().toLowerCase();
+  const wordsArray = words.split('_');
+  let camelCase = [];
+  camelCase.push(wordsArray[0]);
+  camelCase.push(wordsArray[1][0].toUpperCase() + wordsArray[1].slice(1));
+  const joined = camelCase.join('');
+  console.log(joined);
+};
+
+camelCaseConvert('');
+
+const textarea = document.querySelector('textarea');
+const button = document.querySelector('button');
+
+//Guy's solution
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
