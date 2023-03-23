976740067c3e1bb9ed4a47f36a3d1c762ee1575c
sopcoerik <sopcoerik@gmail.com>
2023-03-23 09:46:51 +0200

    JavaScript Fundamentals P1 Challs

--- Recap/JavaScript Fundamentals Part 1/main.js
+++ Recap/JavaScript Fundamentals Part 1/main.js
@@ -2,20 +2,59 @@
 
 // Coding Challenge #1
 
-const markMass = 95;
+/*const markMass = 95;
 const markHeight = 1.88;
 
 const johnMass = 85;
 const johnHeight = 1.76;
 
-const calculateMarkBMI = markMass / markHeight ** 2;
-const calculateJohnBMI = johnMass / johnHeight ** 2;
+const markBMI = (calculateMarkBMI = markMass / markHeight ** 2);
 
-const markBMI = calculateMarkBMI;
-const johnBMI = calculateJohnBMI;
+const johnBMI = (calculateJohnBMI = johnMass / johnHeight ** 2);
 
 const hasMarkGotHigherBMI = markBMI > johnBMI;
 
 console.log(`Mark's BMI: ${markBMI}
 John's BMI: ${johnBMI}
 Is Mark's BMI higher? ${hasMarkGotHigherBMI ? "Yes" : "No"}`);
+
+// Coding Challenge #2
+
+if (markBMI > johnBMI) {
+  console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})`);
+} else {
+  console.log(`John's BMI(${johnBMI}) is higher than Mark's(${markBMI})`);
+}*/
+
+// Coding Challenge #3
+
+/*const scoreDolphins = (calcScoreDolphins = (97 + 112 + 101) / 3);
+
+const scoreKoalas = (calcScoreKoalas = (109 + 95 + 106) / 3);
+
+console.log(calcScoreDolphins, calcScoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100)
+  console.log(
+    `Dolphins win with a score difference of ${scoreDolphins - scoreKoalas}`
+  );
+else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100)
+  console.log(
+    `Koalas win with a score difference of ${scoreKoalas - scoreDolphins}`
+  );
+else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+)
+  console.log(`Draw`);
+else console.log(`No winner`);*/
+
+// Coding Challenge #4
+
+/*const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);*/
