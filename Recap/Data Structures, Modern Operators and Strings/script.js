4ab8aeb07cd5d008e30715655bdcf04ab786cc91
sopcoerik <sopcoerik@gmail.com>
2023-03-31 09:29:13 +0300

    Data Structs etc. Coding Chall. #2

--- Recap/Data Structures, Modern Operators and Strings/script.js
+++ Recap/Data Structures, Modern Operators and Strings/script.js
@@ -5,12 +5,29 @@ const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  //ES6 Enhanced Object literals
+  openingHours,
 
   order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
@@ -36,22 +53,31 @@ const restaurant = {
   orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient, otherIngredients);
   },
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
 };
+
+// property names
+
+const properties = Object.keys(openingHours);
+let openStr = `We are open on ${properties.length} days: `;
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+//property values
+const values = Object.values(openingHours);
+console.log(values);
+
+//Entire object
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+// Optional chaining
+console.log(restaurant.openingHours.mon?.open);
 
 restaurant.orderDelivery({
   time: '22:30',
@@ -93,8 +119,8 @@ const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
 
 // Destructuring objects
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+const { name, openingHours: hours1, categories } = restaurant;
+console.log(name, hours1, categories);
 
 const {
   name: restaurantName,
@@ -273,3 +299,74 @@ printGoals(...game.scored);
 (team1 < team2 && console.log(`Team 1 is more likely to win!`)) ||
   (team2 < team1 && console.log(`Team 2 is more likely to win!`)) ||
   (team1 === team2 && console.log(`Both teams' odds to win are equal!`));
+
+// for of loop
+const menu3 = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const food of menu3) console.log(food);
+for (const [index, food] of menu3.entries())
+  console.log(`${index + 1}: ${food}`);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? `closed`;
+  console.log(`On ${day},we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+const users = [
+  {
+    name: 'John',
+    email: 'hello@john.org',
+  },
+];
+
+const users2 = [];
+
+console.log(users[0]?.name ?? 'User array empty');
+
+if (users2.length > 0) console.log(users2[0].name);
+else console.log('user array empty');
+
+// Coding Challenge #2
+
+//1
+for (const [playerNumber, player] of game.scored.entries()) {
+  console.log(`Goal ${playerNumber + 1}: ${player}`);
+}
+
+//2
+const objVals = Object.values(game.odds);
+let sum = 0;
+for (const odd of objVals) {
+  sum += odd;
+}
+console.log(sum / objVals.length);
+
+//3
+for (const [key, value] of Object.entries(game.odds)) {
+  console.log(
+    `Odd of ${
+      key === 'team1'
+        ? game.team1
+        : key === 'x'
+        ? 'Draw'
+        : key === 'team2'
+        ? game.team2
+        : 0
+    }: ${value}`
+  );
+}
+
+//4
+const scorers = {};
+
+game.scored.forEach(scorer =>
+  scorers[scorer] ? scorers[scorer]++ : (scorers[scorer] = 1)
+);
+
+console.log(scorers);
