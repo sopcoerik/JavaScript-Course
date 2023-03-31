c5f7e34e909d0827d70595650925c65f4a2f1f1b
sopcoerik <sopcoerik@gmail.com>
2023-03-31 11:30:29 +0300

    Data Structs etc Coding Chall. #3

--- Recap/Data Structures, Modern Operators and Strings/script.js
+++ Recap/Data Structures, Modern Operators and Strings/script.js
@@ -370,3 +370,111 @@ game.scored.forEach(scorer =>
 );
 
 console.log(scorers);
+
+// Sets
+
+const ordersSet = new Set(['Pasta', 'Pizza', 'Risotto', 'Pizza', 'Pasta']);
+console.log(ordersSet);
+console.log(new Set('Jonass'));
+console.log(ordersSet.size);
+console.log(ordersSet.has('Bread'));
+console.log(ordersSet.has('Pizza'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+console.log(ordersSet);
+
+for (const order of ordersSet) console.log(order);
+
+//Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(
+  new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size
+);
+
+// Maps
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open')
+  .set(false, 'We are close');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+console.log(rest.size);
+const arr4 = [1, 2];
+rest.set(arr4, 'Test');
+console.log(rest.get(arr4));
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try Again!'],
+]);
+
+console.log(question);
+
+//Convert object to Map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Quizz app
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+// console.log(answer);
+// console.log(question.get(question.get('correct') === answer));
+
+//Convert map to array
+console.log([...question]);
+
+// Coding Challenge #3
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+console.log(gameEvents.values());
+const set = new Set([...gameEvents.values()]);
+const events = [...set];
+console.log(events);
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+for (const [key, value] of gameEvents.entries()) {
+  console.log(`${key <= 45 ? '[FIRST HALF]' : '[SECOND HALF]'}: ${value}`);
+}
