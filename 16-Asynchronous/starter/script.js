409dfa652a08edb9d80333701eb9d1d4c0814486
sopcoerik <sopcoerik@gmail.com>
2023-04-05 09:00:43 +0300

    Asynchronous

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -32,59 +32,143 @@ const countriesContainer = document.querySelector('.countries');
 //   });
 // };
 
-const getCountryAndNeighbour = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
+// const getCountryAndNeighbour = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
 
+//     console.log(data);
+
+//     const html = `<article class="${data.flag}">
+//   <img class="country__img" src="" />
+//   <div class="country__data">
+//     <h3 class="country__name">${data.name}</h3>
+//     <h4 class="country__region">${data.region}</h4>
+//     <p class="country__row"><span>👫</span>${(
+//       Number(data.population) / 1000000
+//     ).toFixed(1)} people</p>
+//     <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//     <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//   </div>
+// </article>`;
+
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = 1;
+//   });
+// };
+// getCountryAndNeighbour('romania');
+
+// const whereAmI = (lat, lng) => {
+//   fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en
+// `)
+//     .then(res => {
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.countryName}`);
+//     });
+// };
+
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.475);
+
+// const lotteryPromis = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (Math.random() >= 0.5) {
+//       resolve('you WIN!');
+//     } else {
+//       reject(new Error('you LOST'));
+//     }
+//   }, 2000);
+// });
+
+// lotteryPromis.then(res => console.log(res)).catch(err => console.log(err));
+
+// Coding Challenge #2
+
+// const wait = function (seconds) {
+//   return new Promise(resolve => {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// const imgContainer = document.querySelector('.images');
+
+// const createImage = imgPath => {
+//   return new Promise((resolve, reject) => {
+//     const img = document.createElement('img');
+//     img.src = imgPath;
+
+//     img.addEventListener('load', () => {
+//       imgContainer.append(img);
+//       resolve(img);
+//     });
+
+//     img.addEventListener('error', () => {
+//       reject(new Error('image not found'));
+//     });
+//   });
+// };
+
+// let currentImg;
+
+// createImage(`img/img-1.jpg`)
+//   .then(img => {
+//     currentImg = img;
+//     console.log(`Image 1 loaded`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log(`Image 2 loaded`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-3.jpg');
+//   })
+//   .then(img => {
+//     currentImg = img;
+//     console.log(`Image 3 loaded`);
+//     return wait(2);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return wait(2);
+//   })
+//   .catch(err => console.log(err));
+
+// Consuming Promises with Async/Await
+
+const whereAmI = async country => {
+  try {
+    fetch(`https://restcountries.com/v2/name/${country}`).then(res =>
+      console.log(res)
+    );
+
+    const res = await fetch(`https://restcountries.com/v2/name/${country}`);
+    const data = await res.json();
     console.log(data);
-
-    const html = `<article class="${data.flag}">
-  <img class="country__img" src="" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      Number(data.population) / 1000000
-    ).toFixed(1)} people</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-</article>`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
-getCountryAndNeighbour('romania');
+whereAmI('portugal');
+console.log(`FIRST`);
 
-const whereAmI = (lat, lng) => {
-  fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en
-`)
-    .then(res => {
-      return res.json();
-    })
-    .then(data => {
-      console.log(data);
-      console.log(`You are in ${data.city}, ${data.countryName}`);
-    });
-};
-
-whereAmI(52.508, 13.381);
-whereAmI(19.037, 72.873);
-whereAmI(-33.933, 18.475);
-
-const lotteryPromis = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (Math.random() >= 0.5) {
-      resolve('you WIN!');
-    } else {
-      reject(new Error('you LOST'));
-    }
-  }, 2000);
-});
-
-lotteryPromis.then(res => console.log(res)).catch(err => console.log(err));
+// try {
+//   let y = 1;
+//   const x = 2;
+//   y = 3;
+// } catch (err) {
+//   alert(err.message);
+// }
