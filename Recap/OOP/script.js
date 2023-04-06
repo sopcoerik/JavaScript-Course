3098020f16a6ee9cd3c1e1be446c268356774951
sopcoerik <sopcoerik@gmail.com>
2023-04-06 23:31:43 +0300

    OOP Coding Challenge #1

---
+++ Recap/OOP/script.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Coding Challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+mercedes.brake();
+mercedes.brake();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
+mercedes.accelerate();
