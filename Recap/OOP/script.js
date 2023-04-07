2bfd8d0c3c605101c3e7126b06ae8d54cc526f20
sopcoerik <sopcoerik@gmail.com>
2023-04-07 12:37:44 +0300

    OOP Coding Challenge #3

--- Recap/OOP/script.js
+++ Recap/OOP/script.js
@@ -63,3 +63,46 @@
 // ford.brake();
 // ford.speedUS;
 // ford.speedUS = 120;
+
+// Coding Challenge #3
+
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+
+// Car.prototype.accelerate = function () {
+//   this.speed += 10;
+//   console.log(this.speed);
+// };
+
+// Car.prototype.brake = function () {
+//   this.speed -= 5;
+//   console.log(this.speed);
+// };
+
+// const EV = function (make, speed, charge) {
+//   Car.call(this, make, speed);
+//   this.charge = charge;
+// };
+
+// EV.prototype = Object.create(Car.prototype);
+
+// EV.prototype.chargeBattery = function (chargeLevel) {
+//   this.charge = chargeLevel;
+//   console.log(`Battery now charged to ${this.charge}%`);
+// };
+
+// EV.prototype.accelerate = function () {
+//   this.speed += 20;
+//   --this.charge;
+//   console.log(
+//     `${this.make} is going now with ${this.speed}km/h, with a charge of ${this.charge}%`
+//   );
+// };
+
+// const tesla = new EV(`Tesla`, 120, 23);
+
+// tesla.accelerate();
+// tesla.brake();
+// tesla.chargeBattery(90);
