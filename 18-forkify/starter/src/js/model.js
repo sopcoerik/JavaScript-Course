02a6972fc1d48474cb625c318d631d3c898e9187
Sorin Bogdan <38883435+sorinbogdan95@users.noreply.github.com>
2023-04-20 09:20:58 +0000

    Update model.js

--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,87 +1,91 @@
-class Model {
-  state = {
-    recipe: {},
-    bookmarks: [],
-    search: {
-      query: '',
-      results: [],
-      // page: 1
-    },
-  };
+export const state = {
+  recipe: {},
+  bookmarks: [],
+  search: {
+    query: '',
+    results: [],
+    // page: 1
+  },
+};
 
-  // update following functions to update the state when called.
-  getRecipeData = async function (recipeId) {
-    try {
-      const getData = await fetch(
-        `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId.slice(1)}`
-      );
-
-      const data = await getData.json();
-
-      let { recipe } = data.data;
-
-      this.state.recipe = {
-        cookingTime: recipe.cooking_time,
-        recipeId: recipe.recipeId,
-        imageUrl: recipe.image_url,
-        ingredients: recipe.ingredients,
-        publisher: recipe.publisher,
-        servings: recipe.servings,
-        sourceUrl: recipe.source_url,
-        title: recipe.title,
-      };
-    } catch (err) {
-      throw new Error(err);
-    }
-  };
-  // } catch (err) {
-  //   // recipeView.showError() -> but don't call it here!
-  //   // the controller handles the errors
-  //   // the controller is the glue between model and view
-  //   // DON'T CALL VIEW OR CONTROLLER FUNCTIONS IN THE MODEL
-  //   document.querySelector('.recipe').textContent = '';
-  //   const html = `
-  //   <div class="error">
-  //     <div>
-  //       <svg>
-  //         <use href="${icons}#icon-alert-triangle"></use>
-  //       </svg>
-  //     </div>
-  //     <p>No recipes found for your query. Please try again!</p>
-  //   </div>
-  //   `;
-  //   document.querySelector('.recipe').insertAdjacentHTML('afterbegin', html);
-  // }
-
-  getRecipesFromSearch = async function () {
-    this.state.query = document.querySelector('.search__field').value;
-    const getResults = fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes?search=${this.state.query}`
+export const getRecipeData = async function (recipeId) {
+  try {
+    const getData = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId.slice(1)}`
     );
-    const results = await getResults;
-    const results2 = await results.json();
-    const { recipes: recipesArr } = results2.data;
 
-    recipesArr.forEach(recipe => this.state.results.push(recipe));
-  };
+    const data = await getData.json();
 
-  // updateServings
-  updateRecipeServings(servings) {
-    this.state.recipe.servings = servings;
+    let { recipe } = data.data;
+
+    state.recipe = {
+      cookingTime: recipe.cooking_time,
+      recipeId: recipe.recipeId,
+      imageUrl: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+  } catch (err) {
+    throw new Error(err);
   }
+};
+// } catch (err) {
+//   // recipeView.showError() -> but don't call it here!
+//   // the controller handles the errors
+//   // the controller is the glue between model and view
+//   // DON'T CALL VIEW OR CONTROLLER FUNCTIONS IN THE MODEL
+//   document.querySelector('.recipe').textContent = '';
+//   const html = `
+//   <div class="error">
+//     <div>
+//       <svg>
+//         <use href="${icons}#icon-alert-triangle"></use>
+//       </svg>
+//     </div>
+//     <p>No recipes found for your query. Please try again!</p>
+//   </div>
+//   `;
+//   document.querySelector('.recipe').insertAdjacentHTML('afterbegin', html);
+// }
 
-  //addBookmark
-  addRecipeToBookmarks(recipe) {
-    this.state.bookmarks.push(recipe);
-  }
+// todo: will move this somewhere else: document.querySelector('.search__field').value;
+export const getRecipesFromSearch = async query => {
+  state.query = query;
+  const getResults = fetch(
+    `https://forkify-api.herokuapp.com/api/v2/recipes?search=${state.query}`
+  );
+  const results = await getResults;
+  const resultsJson = await results.json();
+  const { recipes: recipesArr } = resultsJson.data;
 
-  // deleteBookmark
-  deleteRecipeFromBookmarks(recipe) {
-    const recipeIndex = this.state.bookmarks.findIndex(
-      recipeCurr => recipe.id === recipeCurr.id
-    );
-    this.state.bookmarks.splice(recipeIndex, 1);
-  }
-}
+  //TODO: use map to get new state. transform the imageurl to the correct atrribute
+  recipesArr.forEach(recipe => state.results.push(recipe));
+};
 
-export default new Model();
+// updateServings
+export const updateRecipeServings = servings => {
+  state.recipe.servings = servings;
+  // TODO: ADD THE logic that calculates the recipe.ingredients quantity here. update the recipe ingredients state.
+};
+
+//addBookmark
+export const addRecipeToBookmarks = recipe => {
+  state.bookmarks.push(recipe);
+  // where do you mark the current recipe as bookmarked?  (maybe add state.recipe.bookmarked)
+
+  // where do you persist bookmarks ?
+};
+
+// deleteBookmark
+export const deleteRecipeFromBookmarks = recipe => {
+  const recipeIndex = state.bookmarks.findIndex(
+    recipeCurr => recipe.id === recipeCurr.id
+  );
+  state.bookmarks.splice(recipeIndex, 1);
+  // persistence of bookmarks ?
+};
+
+// TODO: init function which initializes the bookmarks from local storage
