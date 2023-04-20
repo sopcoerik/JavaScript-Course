e6e24cb547755ac054f4ea6103eb3482fc97bce0
sopcoerik <sopcoerik@gmail.com>
2023-04-20 23:25:51 +0300

    Implemented update method to View.js

--- 18-forkify/starter/dist/index.e37f48ea.js
+++ 18-forkify/starter/dist/index.e37f48ea.js
@@ -3127,10 +3127,25 @@ class View {
     _parentElement;
     _message;
     render(data) {
+        if (!data || Array.isArray(data) && data.length === 0) return this.renderError();
         this._data = data;
         const markup = _generateMarkup(); // this function is individual for every view
         this._clear();
         this._parentElement.insertAdjacentHTML("afterbegin", markup);
+    }
+    update(data) {
+        this._data = data;
+        const newMarkup = _generateMarkup(); // at this point it's just a string so we can not compare it to the current dom
+        const newDOMFromNewMarkupString = document.createRange().createContextualFragment(newMarkup); // we first convert the html string to a DOM obj which lives only in memory, not actual part of DOM
+        const newDOMElements = newDOMFromNewMarkupString.querySelectorAll("*"); // and here we take out all the elements from the new DOM object
+        const currentDOMElements = this._parentElement.querySelectorAll("*"); // we take out the current DOM elements from the parent element where we want to update the view with new data
+        newDOMElements.forEach((newEl, i)=>{
+            const currEl = currentDOMElements[i];
+            // Updates based on TEXT-CHANGE
+            if (!newEl.isEqualNode(currEl) && newEl.firstChild.nodeValue.trim() !== "") currEl.textContent = newEl.textContent;
+            if (!newEl.isEqualNode(currEl)) Array.from(newDOMElements.attributes).forEach(// here we can set the current elements attributes if they change
+            (attribute)=>currEl.setAttributes(attribute.name, attribute.value));
+        });
     }
     _clear() {
         this._parentElement.innerHTML = "";
